47d1ee7ac4d3f1f977b90b1dc59707baf67fe815
fernandoallisson <fernando.alisson@ufpe.br>
2023-05-25 15:39:22 -0300

    Iniciando o projeto

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,9 +1,18 @@
 // Remova os comentários a medida que for implementando as funções
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+const TOKEN = import.meta.env.VITE_TOKEN;
+const URL = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=$`;
+
+export const searchCities = async (term) => { // Aqui retorna uma cidade que for digitada no term
+  try {
+    const response = await fetch(URL + term);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error.message;
+  }
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
-//   seu código aqui
+  //   seu código aqui
 };
